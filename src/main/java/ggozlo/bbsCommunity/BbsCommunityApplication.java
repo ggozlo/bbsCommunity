8b8aa722af266699c91b9ea844024490499a7d70
ggozlo <ggozlo@naver.com>
2021-09-22 18:49:16 +0900

    BBSCommunity initial commit

---
+++ src/main/java/ggozlo/bbsCommunity/BbsCommunityApplication.java
@@ -0,0 +1,13 @@
+package ggozlo.bbsCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ggozlo/bbsCommunity/BbsCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package ggozlo.bbsCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
